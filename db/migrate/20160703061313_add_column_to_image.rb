8ad29bf8a2b895834f2265efa08c621b4d182d31
Harmeet <harmeetsingh301296@gmail.com>
2016-07-03 06:13:56 +0000

    added foriegn key

--- db/migrate/20160703061313_add_column_to_image.rb
+++ db/migrate/20160703061313_add_column_to_image.rb
@@ -1,5 +1,5 @@
 class AddColumnToImage < ActiveRecord::Migration
   def change
-    add_reference :images, :mobile, index: true, foreign_key: true
+    add_reference :images, :user, index: true, foreign_key: true
   end
 end
